6a357d8e9aed2ad5bb7d70a3e90556684a74df92
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-04-19 18:04:07 +0200

    Add and update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './sass/style.scss';
+import { plusDivs } from './modules/slideShow.js';
+import FetchGamesApi from './modules/getGames.js';
 import './assets/images/image1.png';
 import './assets/images/image11.png';
 import './assets/images/image2.png';
@@ -12,65 +14,11 @@ import './assets/images/image13.png';
 import './assets/images/image15.png';
 import './assets/images/image17.png';
 
-/*
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
-    'X-RapidAPI-Key': '838b0eeb18msh840203b450993abp154a9fjsnea041f3d274c',
-  },
-};
-
-fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter', options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
- */
-let slideIndex = 1;
-
-function showDivs(n) {
-  let i;
-  const x = document.getElementsByClassName('mySlides');
-  if (n > x.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i += 1) {
-    x[i].style.display = 'none';
-  }
-  x[slideIndex - 1].style.display = 'block';
-}
-
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
 const arrowRight = document.querySelector('.b-display-right');
 
-arrowRight.addEventListener('click', plusDivs(1));
+arrowRight.addEventListener('click', () => plusDivs(1));
 
 const arrowLeft = document.querySelector('.b-display-left');
-arrowLeft.addEventListener('click', plusDivs(-1));
+arrowLeft.addEventListener('click', () => plusDivs(-1));
 
-let myIndex = 0;
-
-function carousel() {
-  let i;
-  const x = document.getElementsByClassName('mySlides');
-  for (i = 0; i < x.length; i += 1) {
-    x[i].style.display = 'none';
-  }
-  myIndex += 1;
-  if (myIndex > x.length) {
-    myIndex = 1;
-  }
-  x[myIndex - 1].style.display = 'block';
-
-  setTimeout(carousel, 2500); // Change image every 2.5 seconds
-}
-
-carousel();
+FetchGamesApi.executor();
---
+++ src/modules/getGames.js
@@ -0,0 +1,64 @@
+export default class FetchGamesApi {
+  constructor() {
+
+  }
+
+  static getGamesFetch = async => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
+        'X-RapidAPI-Key': '838b0eeb18msh840203b450993abp154a9fjsnea041f3d274c',
+      },
+    };
+    return fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter', options)
+    .then((response) => response.json())
+    }
+
+  static gamesRender = async (gamesData) => {
+    gamesData.forEach(async (e) => {
+      // const likes = await updateLikes(e.id);
+      const gameList = document.querySelector('.cards-section');
+      gameList.innerHTML += `
+      <div class="card">
+      <img src="${e.thumbnail}" alt="" class="card-images" />
+      <div class="content">
+       <div class="name-div">
+        <h2>${e.title}</h2>
+        <span class="likes"><i class="fas fa-heart fa-lg"></i></span>
+       </div>
+       <span class="number-of-likes">5 Likes</span>
+       <p class="description">${e.short_description}</p>
+       <button class="comment">Comments</button>
+      </div>
+     </div>`;
+    });
+  }
+  
+  
+  static executor = async () => {
+    const movie = await this.getGamesFetch();
+    const gamesData = movie.slice(0, 40).map((item) => item);
+    await this.gamesRender(gamesData);
+    };
+
+  static gamesCounter = (data) => {
+    const countItems = data.length;
+    return countItems;
+    };
+  }
+/* export default class Overpass {
+static options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
+    'X-RapidAPI-Key': '838b0eeb18msh840203b450993abp154a9fjsnea041f3d274c',
+  },
+};
+
+static getGames() { fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter', options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+}
+} */
