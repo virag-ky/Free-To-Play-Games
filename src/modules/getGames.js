9f76a10d2d552a9603c339b2b538fabd338bb6f9
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-04-21 21:18:21 +0200

    Add backend functionalities

--- src/modules/getGames.js
+++ src/modules/getGames.js
@@ -1,9 +1,7 @@
+import Likes from '../modules/getLikes.js';
 export default class FetchGamesApi {
-  constructor() {
 
-  }
-
-  static getGamesFetch = async => {
+  static getGamesFetch = async () => {
     const options = {
       method: 'GET',
       headers: {
@@ -15,50 +13,58 @@ export default class FetchGamesApi {
     .then((response) => response.json())
     }
 
+    static executor = async () => {
+      const movie = await this.getGamesFetch();
+      const gamesData = movie.slice(0, 40).map((item) => item);
+      await this.gamesRender(gamesData);
+      };
+  
   static gamesRender = async (gamesData) => {
     gamesData.forEach(async (e) => {
-      // const likes = await updateLikes(e.id);
-      const gameList = document.querySelector('.cards-section');
+       const likes = await Likes.updateLikes(e.id);
+      const gameList = document.getElementById('cards');
       gameList.innerHTML += `
       <div class="card">
-      <img src="${e.thumbnail}" alt="" class="card-images" />
+      <img src="${e.thumbnail}" alt="Thumbnail" class="card-images">
       <div class="content">
-       <div class="name-div">
-        <h2>${e.title}</h2>
-        <span class="likes"><i class="fas fa-heart fa-lg"></i></span>
-       </div>
-       <span class="number-of-likes">5 Likes</span>
-       <p class="description">${e.short_description}</p>
-       <button class="comment">Comments</button>
+      <div class="name-div">
+      <h2>${e.title}</h2>
+      <div class="like-wrapper"><i class="far fa-heart likes" id="love-icon"></i>
+      <span class="likes" id="${e.id}">${likes}</span></div>
       </div>
-     </div>`;
-    });
-  }
-  
-  
-  static executor = async () => {
-    const movie = await this.getGamesFetch();
-    const gamesData = movie.slice(0, 40).map((item) => item);
-    await this.gamesRender(gamesData);
-    };
+      <p class="description">${e.short_description}</p>
+      <button id="${e.id}" class="comment-btn">Comments</button>
+      </div>
+     </div>
+      `;
+        })
+      };
 
   static gamesCounter = (data) => {
     const countItems = data.length;
     return countItems;
     };
-  }
-/* export default class Overpass {
-static options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
-    'X-RapidAPI-Key': '838b0eeb18msh840203b450993abp154a9fjsnea041f3d274c',
-  },
-};
 
-static getGames() { fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter', options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+    static createGameID = async () => {
+     const requestURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+     const result = await fetch(`${requestURL}apps/`, {
+       method: 'POST',
+       headers: {
+         'Content-type': 'application/json; charset=UTF-8',
+       },
+     })
+     .then((response) => response.text())
+     return result;
+   };
+
+   static createGameID2 = async () => {
+    const requestURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+    const options = {
+    method: 'POST',
+  };
+  fetch(`${requestURL}apps/`, options )
+      .then( response => response.json() )
+      .then( response => {
+      console.log(response)});
+   }
 }
-} */
